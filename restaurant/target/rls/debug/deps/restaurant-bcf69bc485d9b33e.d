8f800312c4ccedb36192c2b8d0c8faeea16484ad
trancongduynguyen1997 <tcdnguyen1997@gmail.com>
2022-01-20 16:03:08 +0700

    Chap 7

---
+++ restaurant/target/rls/debug/deps/restaurant-bcf69bc485d9b33e.d
@@ -0,0 +1,5 @@
+/home/nguyen/workspace/prac/rust/rush-rust/restaurant/target/rls/debug/deps/restaurant-bcf69bc485d9b33e.rmeta: src/lib.rs
+
+/home/nguyen/workspace/prac/rust/rush-rust/restaurant/target/rls/debug/deps/restaurant-bcf69bc485d9b33e.d: src/lib.rs
+
+src/lib.rs:
