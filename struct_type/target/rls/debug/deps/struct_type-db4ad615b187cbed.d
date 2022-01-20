93cdb2854885f8022e5eaa4bbcafd163fc354e3e
trancongduynguyen1997 <tcdnguyen1997@gmail.com>
2022-01-20 10:21:34 +0700

    Chap 5

---
+++ struct_type/target/rls/debug/deps/struct_type-db4ad615b187cbed.d
@@ -0,0 +1,5 @@
+/home/nguyen/workspace/prac/rust/rush-rust/struct_type/target/rls/debug/deps/struct_type-db4ad615b187cbed.rmeta: src/main.rs
+
+/home/nguyen/workspace/prac/rust/rush-rust/struct_type/target/rls/debug/deps/struct_type-db4ad615b187cbed.d: src/main.rs
+
+src/main.rs:
