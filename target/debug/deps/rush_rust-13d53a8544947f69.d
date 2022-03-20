965a4d7e4aa5813230137dc7353a23dcd139d760
Tran Nguyen <tcdnguyen1997@gmail.com>
2022-03-20 17:00:26 +0700

    Remove unused target

--- target/debug/deps/rush_rust-13d53a8544947f69.d
+++
@@ -1,5 +0,0 @@
-/Users/trannguyen/Workspaces/rush-rust/target/debug/deps/rush_rust-13d53a8544947f69.rmeta: main.rs
-
-/Users/trannguyen/Workspaces/rush-rust/target/debug/deps/rush_rust-13d53a8544947f69.d: main.rs
-
-main.rs:
--- target/debug/deps/rush_rust-710406ecd740be33.d
+++
@@ -1,5 +0,0 @@
-/Users/trannguyen/Workspaces/rush-rust/target/debug/deps/rush_rust-710406ecd740be33.rmeta: main.rs
-
-/Users/trannguyen/Workspaces/rush-rust/target/debug/deps/rush_rust-710406ecd740be33.d: main.rs
-
-main.rs:
